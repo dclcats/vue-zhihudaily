2511c0d5fcc7ffdba9e507da4eced3faa90f29b5
Chris <chichucan@126.com>
2017-05-15 17:25:07 +0800

    通过heroku解决跨域

--- src/api/config.js
+++ src/api/config.js
@@ -10,6 +10,6 @@ const config = {
 }
 
 
-// export const API_ROOT = config.protocol.concat(config.site)
+export const API_ROOT = config.protocol.concat(config.site)
 // export const API_ROOT = config1.protocol.concat(config1.common)
-export const API_ROOT = '/api'
+// export const API_ROOT = '/api'
